803c4c57dbb170117f9dd9c230a9009308c7f70c
carraes <carraeshb@gmail.com>
2023-11-13 01:45:02 -0300

    chore: add initial data to db

--- src/main/java/com/carraes/dndsys/DatabaseLoader.java
+++ src/main/java/com/carraes/dndsys/DatabaseLoader.java
@@ -1,7 +1,13 @@
 package com.carraes.dndsys;
 
+import com.carraes.dndsys.models.Hero;
 import com.carraes.dndsys.models.Monstro;
+import com.carraes.dndsys.models.Npc;
+import com.carraes.dndsys.models.Weapon;
+import com.carraes.dndsys.services.HeroService;
 import com.carraes.dndsys.services.MonstroService;
+import com.carraes.dndsys.services.NpcService;
+import com.carraes.dndsys.services.WeaponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +16,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseLoader {
   @Autowired private MonstroService monstroService;
+  @Autowired private WeaponService weaponService;
+  @Autowired private HeroService heroService;
+  @Autowired private NpcService npcService;
 
   @Bean
   CommandLineRunner initDatabase() {
@@ -23,6 +32,14 @@ public class DatabaseLoader {
               7,
               5,
               false));
+
+      weaponService.create(new Weapon(10, "Corte", "Espada curta"));
+
+      heroService.create(
+          new Hero("Adam", "Um guerreiro com uma espada curta", 1, 100, "Guerreiro"));
+      heroService.assignWeapon(1, 1);
+
+      npcService.create(new Npc("Alan", "Um mercante viajante", 1, "Ferreiro", true));
     };
   }
 }
